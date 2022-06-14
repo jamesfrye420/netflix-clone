f8c0949d3eb2814aa0d121d7c5c4248735df7083
jamesfrye420 <avikpaulsept2002@gmail.com>
2022-06-14 19:19:14 +0530

    changes to .gitignore

--- backend/dist/routes/addfaq.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const faqs_1 = require("../controller/faqs");
-const router = (0, express_1.Router)();
-router.post('/faqs', faqs_1.postFaq);
-exports.default = router;
-//# sourceMappingURL=addfaq.js.map
